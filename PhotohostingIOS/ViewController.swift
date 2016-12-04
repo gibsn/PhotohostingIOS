9a7c96bd4a9ebf3a4583f7f91d938c0c176f2f0a
Kirill Alekseev <kirill-8@yandex.ru>
2016-12-05 00:49:15 +0300

    Initial Commit

---
+++ PhotohostingIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotohostingIOS
+//
+//  Created by Кирилл Алексеев on 05.12.16.
+//  Copyright © 2016 Кирилл Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
