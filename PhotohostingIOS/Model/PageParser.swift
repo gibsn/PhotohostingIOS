a26500ee45fc0f4678e1e2afdacd65c50aed91ae
Kirill Alekseev <kirill-8@yandex.ru>
2016-12-20 02:11:40 +0300

    started developing GUI

---
+++ PhotohostingIOS/Model/PageParser.swift
@@ -0,0 +1,9 @@
+//
+//  PageParser.swift
+//  PhotohostingIOS
+//
+//  Created by Кирилл Алексеев on 05.12.16.
+//  Copyright © 2016 Кирилл Алексеев. All rights reserved.
+//
+
+import Foundation
---
+++ PhotohostingIOS/ViewControllers/FullPhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FullPhotoViewController.swift
+//  PhotohostingIOS
+//
+//  Created by Kirill Alekseev on 20.12.16.
+//  Copyright © 2016 Kirill Alekseev. All rights reserved.
+//
+
+import UIKit
+
+class FullPhotoViewController: UIViewController {
+    @IBAction func ActionButton(_ sender: UIBarButtonItem) {
+    }
+
+    override func viewDidLoad() {
+                super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
