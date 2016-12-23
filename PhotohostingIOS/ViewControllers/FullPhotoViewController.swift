3a88f19c5c9a200434e59039467f0cc07ddc0d78
Kirill Alekseev <kirill.alekseev@corp.mail.ru>
2016-12-23 17:22:06 +0300

    Added activity indicator for the orignal image view

--- PhotohostingIOS/ViewControllers/FullPhotoViewController.swift
+++ PhotohostingIOS/ViewControllers/FullPhotoViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class FullPhotoViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     public var photoIndex: Int = -1
     public var photosArr: [Photo]?
     
@@ -21,7 +22,12 @@ class FullPhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.activityIndicator.startAnimating()
+        self.activityIndicator.hidesWhenStopped = true
+        
         let url = URL(string: self.photosArr![photoIndex].originalUrl)!
-        imgView.af_setImage(withURL: url)
+        imgView.af_setImage(withURL: url) { _ in
+            self.activityIndicator.stopAnimating()
+        }
     }
 }
